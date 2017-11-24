66f77421544185269c6c3b5e164cc3d536abb00e
Matheus <rockytheus_23@hotmail.com>
2017-11-24 20:31:50 -0200

    Inserção mask inputs

--- SmartUP/scripts/script.js
+++ SmartUP/scripts/script.js
@@ -4,11 +4,13 @@ $(".button-collapse").sideNav();
 // Controles das mascaras
 $('.date').mask('00/00/0000', { placeholder: "__/__/____" });
 $('.time').mask('00:00:00', { placeholder: "00:00:00" });
+$('.datetime').mask('00/00/0000 00:00:00', { placeholder: "00/00/0000 00:00:00" });
 $('.cep').mask('00000-000', { placeholder: "00000-000" });
 $('.phone_with_ddd').mask('(00) 0000-0000', { placeholder: "(xx) xxxx-xxxx" });
 $('.celphones_with_ddd').mask('(00) 00000-0000', { placeholder: "(xx) xxxxx-xxxx" });
 $('.cnpj').mask('00.000.000/0000-00', { reverse: true, placeholder: "00.000.000/0000-00" });
 $('.cpf').mask('000.000.000-00', { reverse: true, placeholder: "000.000.000-00" });
+$('.rg').mask('00.000.000-0', { reverse: true, placeholder: "00.000.000-0" });
 $('.money').mask('#.##0,00', { reverse: true, placeholder: "R$ 0,00" });
 //Mascara especial para campo celular ou fixo
 var SPMaskBehavior = function (val) {
